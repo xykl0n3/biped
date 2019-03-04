2e32d4d12bf7068770ceb64d66031d5deaab893b
xykl0n3 <sishirmehta98@gmail.com>
2019-03-04 20:33:38 +0530

    Add files via upload

--- biped/func.py
+++ biped/func.py
@@ -1,7 +1,46 @@
 import dxl2
-from run import read,do
+import time
+from dxl2 import register
+from read import read_file
 
-#read()
-do(walk)
 
+
+
+conn = dxl2.Connection("/dev/ttyUSB0")
+conn.open_port()
+
+
+###### uncomment to check present position ########
+# for i in range (1,11):
+#         m = dxl2.Motor(conn, i, dxl2.MotorType.AX)
+#         print(m.read(register.Instruction.PRESENT_POSITION))        
+
+
+####### gait ######
+
+
+
+
+def do(function):
+        
+
+        pos = read_file(function)
+        for list in pos:
+                for i in range (1,11):
+                        m = dxl2.Motor(conn, i, dxl2.MotorType.AX)
+
+                        ####### change moving speed #######
+                        m.write(register.Instruction.MOVING_SPEED,40)
+                        
+                        m.write(register.Instruction.GOAL_POSITION, list[i-1])
+
+
+
+###### to read present position ######
+
+def read():     
+        for i in range (1,11):
+                m = dxl2.Motor(conn, i, dxl2.MotorType.AX)
+                print(m.read(register.Instruction.PRESENT_POSITION))
+        
 
